958b5ca1c162bf707a34db6ab2a6f2c710c99a3d
benscott.co.uk <ben@benscott.co.uk>
2019-05-08 14:16:34 +0100

    Adding proximity

--- proximity.py
+++ proximity.py
@@ -1,10 +1,18 @@
 import pandas as pd
-from base import Session, Page, Trait, TraitPage
 import datetime
 from sqlalchemy import and_
 
+from base import Session, Page, TraitView
+from ocr import OCR
+
 start = datetime.datetime.now()
 print(f"start time: {start}")
+
+session = Session()
+
+for trait in session.query(TraitView).limit(2):
+    ocr_image = OCR(trait.image_url)
+    print(ocr_image.text)
 
 
 end = datetime.datetime.now()
