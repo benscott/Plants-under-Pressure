304d6b28bb23b278bcc0b1b5dd3344f57892c638
benscott.co.uk <ben@benscott.co.uk>
2019-05-08 11:11:33 +0100

    Add proximity

---
+++ proximity.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from base import Session, Page, Trait, TraitPage
+import datetime
+from sqlalchemy import and_
+
+start = datetime.datetime.now()
+print(f"start time: {start}")
+
+
+end = datetime.datetime.now()
+print(f"start time: {end}")
+print(f"duration: {end - start}")
